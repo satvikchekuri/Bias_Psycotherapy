95d3f8ac394cad19db1a5660ad7c6e1ecb68e184
satvikchekuri <satvik.rs@gmail.com>
2022-03-27 19:13:44 -0400

    Updates

--- file_process.py
+++ file_process.py
@@ -1,7 +1,23 @@
 import pandas as pd
+import nltk
+import re
 
 file_name = 'C:/Users/satvi/Downloads/CSS5984_Project/Data/satvik_txt/Abigail McNally.txt'
-f = open(file_name,'r')
-text_0 = f.read()
+file_name1 = 'C:/Users/satvi/Downloads/CSS5984_Project/Data/Abigail McNally.csv'
+text = pd.read_csv(file_name1)
 
-print(text_0)
+text1 = {}
+text1['Sentences'] = list()
+text1['Patient_Gender'] = list()
+text1['Therapist_Gender'] = list()
+for i in text['Column1']:
+    if 'THERAPIST:' in i:
+        j = i.replace('THERAPIST:','')
+        j = j.strip()
+        text1['Sentences'].append(j)
+        text1['Patient_Gender'].append('Male')
+        text1['Therapist_Gender'].append('Female')
+        # print(i)
+text2 = pd.DataFrame({'Sentences':text1['Sentences'], 'Patient_Gender':text1['Patient_Gender'], 'Therapist_Gender':text1['Therapist_Gender']})
+print(text2)
+text2.to_csv('C:/Users/satvi/Downloads/CSS5984_Project/Data/test.csv', sep=",", index=False)
